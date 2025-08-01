d5778df8364002167e141323a9268c3fce4d3beb
Neeraj0308 <ayanpatel.0308@gmail.com>
2025-08-01 11:02:59 +0530

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles.css"; // Optional: Tailwind or basic CSS
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
